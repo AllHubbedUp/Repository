4a8f693df1d464bc85d28bee7e3d53d622de3599
AllHubbedUp <115436491+AllHubbedUp@users.noreply.github.com>
2023-10-11 19:50:00 +0000

    Committed

---
+++ index.py
@@ -0,0 +1,11 @@
+class Dict:
+    
+
+
+class Classy:
+    def __init__(self, name, ifunc):
+        self.name = name
+        self.init = ifunc
+        #Create dict class for storing class details
+    def addfunc(self, func):
+        #Create dynamic structure for list
